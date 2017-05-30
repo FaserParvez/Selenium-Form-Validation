8320ed30693533bfdc7f650738443e9701de4546
FaserParvez <FaserParvez@hotmail.com>
2017-05-30 19:00:26 +0100

    Update formvalidation.js

--- formvalidation.js
+++ formvalidation.js
@@ -197,11 +197,15 @@ $(document).ready(function() {
   }
   // This function will check to see if both the passwords are right.
   function validatebothpass() {
-    if(password1.val() == password2.val() || password2.val() == password1.val()  ) {
+    if(password2.val() !== password1.val()) {
+      confirmPasswordErrorMsg.text("The passwords don't match.");
+      passwordErrorMsg.text("The Passwords don't match.");
+    } else if (password2.val() == password1.val()){
       confirmPasswordErrorMsg.text("OK");
-      passwordErrorMsg.text("Ok");
-    } else {
-      passwordErrorMsg.text("The passwords don't match.");
+      passwordErrorMsg.text("OK");
+      // This will toggle a slide for the error message. 
+      $('#confirmPasswordErrorMsg').slideToggle(1500);
+      $('#confirmPasswordErrorMsg').slideToggle(1500);
     }
   }
   
